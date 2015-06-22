e91e40d50889396eb62e6fe7c9c962453d9b8492
ShamimYousuf <waheedayousuf20@gmail.com>
2015-06-22 11:07:27 +0100

    Convert to Maven project and ass JUnit

--- src/test/java/com/asos/SmokeTest1.java
+++ src/test/java/com/asos/SmokeTest1.java
@@ -1,36 +1,20 @@
 package com.asos;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.Select;
 
-/**
- * Created with IntelliJ IDEA.
- * User: shamimyousuf
- * Date: 19/06/2015
- * Time: 22:59
- * To change this template use File | Settings | File Templates.
- */
 public class SmokeTest1 {
 
-    public static void main(String[] args) {
-        // Create a new instance of the html unit driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-//        WebDriver driver = new HtmlUnitDriver();
-
+    @Test
+    public void verifyPricingRangeFilter(){
         WebDriver driver= new FirefoxDriver();
-//        WebDriver driver = new SafariDriver();
-//        WebDriver driver =   new ChromeDriver();
-        // And now use this to visit Google
-        driver.get("http://www.asos.com");
 
+        driver.get("http://www.asos.com");
 
         Actions actions = new Actions(driver);
         WebElement menuHoverLink = driver.findElement(By.cssSelector(".floor_1 "));
@@ -42,13 +26,9 @@ public class SmokeTest1 {
 
         System.out.print(" linktext is "+ subLink.getTagName());
 
-
-
-
-//        actions.clickAndHold();
         actions.click();
         actions.perform();
-        // Check the title of the page
+
         System.out.println("Page title is: " + driver.getTitle());
 
         System.out.print(driver.findElement(By.id("ctl00_ContentMainPage_ctlCategoryRefine_drpdwnPageSort")).getText());
@@ -59,16 +39,9 @@ public class SmokeTest1 {
         WebElement slider = driver.findElement(By.cssSelector("a.ui-slider-handle"));
         System.out.println("slider is: " + slider.getText());
 
-
-//        actions.moveToElement(slider).clickAndHold();
-//        actions.clickAndHold();
-//        actions.moveByOffset(45,58)    ;
-
-
-
         driver.quit();
-
         driver.close();
     }
+
 
 }
